def5d969e6f7538b0b0afff0d3239cafab076013
Saidabrorxon06 <saidabrorahmadov@icloud.com>
2025-01-05 06:34:05 +0500

    Add files via upload

---
+++ tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://ykmDApr680FJ2-jFJJjw8AQ6gsyDhAYB_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "AH5-Problem solving and decision-making"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;Organization management is based on decisions made by managers and those who work with them. Organizations are made up of managers and groups of workers whom they lead through decision-making. It was previously noted that one of the key roles of a manager is decision-making roles. Decision-making, along with the presence of subordinates, is the hallmark of the manager&amp;#39;s position in the organization.&lt;br&gt;Decision-making is an important part of any management activity. If communication is a kind of core that permeates any activity in the organization, then decision-making is the center around which the life of the organization revolves. A decision is viewed as a product of management activity, and its adoption is viewed as a process leading to the emergence of this product.&lt;/p&gt;"
+};
